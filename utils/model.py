cf1eead131a7a5a675e4fd56508990257c49901d
Jesse <jesseguerrero1991@gmail.com>
2025-03-25 15:49:05 +0000

    update

--- utils/model.py
+++ utils/model.py
@@ -80,19 +80,19 @@ class LSTNowcaster(pl.LightningModule):
              sync_dist=True)
     
     def save_rmse(self, batch, outputs, rmse_list_lst, rmse_list_heatindex):  
-        # print('output', torch.mean(outputs[:, 0:1, :, :]))
-        # print('output', torch.mean(outputs[:, 1:2, :, :]))
-        # mask = batch['target'][:, 0:1, :, :] != -9999
-        # print('target', torch.mean(batch['target'][:, 0:1, :, :][mask]))
-        # print('target', torch.mean(batch['target'][:, 1:2, :, :][mask]))
-        # print("Output shape -> ", outputs.shape)
+        print('output', torch.mean(outputs[:, 0:1, :, :]))
+        print('output', torch.mean(outputs[:, 1:2, :, :]))
+        mask = batch['target'][:, 0:1, :, :] != -9999
+        print('target', torch.mean(batch['target'][:, 0:1, :, :][mask]))
+        print('target', torch.mean(batch['target'][:, 1:2, :, :][mask]))
+        print("Output shape -> ", outputs.shape)
         outputs = TiledGeotiffDataset.denormalize(outputs)      
         targets = TiledGeotiffDataset.denormalize(batch['target'])
-        # print('output', torch.mean(outputs[:, 0:1, :, :]))
-        # print('output', torch.mean(outputs[:, 1:2, :, :]))
-        # mask = batch['target'][:, 0:1, :, :] != -9999
-        # print('target', torch.mean(targets[:, 0:1, :, :][mask]))
-        # print('target', torch.mean(targets[:, 1:2, :, :][mask]))
+        print('output', torch.mean(outputs[:, 0:1, :, :]))
+        print('output', torch.mean(outputs[:, 1:2, :, :]))
+        mask = batch['target'][:, 0:1, :, :] != -9999
+        print('target', torch.mean(targets[:, 0:1, :, :][mask]))
+        print('target', torch.mean(targets[:, 1:2, :, :][mask]))
         mask = batch['mask']
         lst = targets[:, 0:1, :, :]        
         heatIndex = targets[:, 1:2, :, :]
